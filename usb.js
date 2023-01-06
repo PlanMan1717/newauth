26c159ffbf8a1ba25f5f0227704433dfd2aac4e6
PlanMan1717 <floor.tile.zebra@gmail.com>
2023-01-06 18:56:25 -0500

    first commit

---
+++ usb.js
@@ -0,0 +1,29 @@
+async function doWebAuthnCreate() {
+    const publicKeyCredentialCreationOptions = {
+        challenge: Uint8Array.from(
+            "hi", c => c.charCodeAt(0)),
+        rp: {
+            name: "Duo Security",
+            id: "duosecurity.com",
+        },
+        user: {
+            id: Uint8Array.from(
+                "UZSL85T9AFC", c => c.charCodeAt(0)),
+            name: "lee@webauthn.guide",
+            displayName: "Lee",
+        },
+        pubKeyCredParams: [{alg: -7, type: "public-key"}],
+        authenticatorSelection: {
+            authenticatorAttachment: "cross-platform",
+        },
+        timeout: 60000,
+        attestation: "direct"
+    };
+    
+    const credential = await navigator.credentials.create({
+        publicKey: publicKeyCredentialCreationOptions
+    });
+
+    console.log(credential);
+    
+}
